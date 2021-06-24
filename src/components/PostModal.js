62e0f0e624c1cb5a7e7d77dc629cabd44d486527
Hunter <hunterwhitedev@gmail.com>
2021-06-24 10:44:02 -0400

    Share buttons done, started text area

--- src/components/PostModal.js
+++ src/components/PostModal.js
@@ -1,21 +1,45 @@
 import styled from "styled-components";
+import { useState } from "react";
 
 const PostModal = (props) => {
+  const [editorText, setEditorText] = useState("");
+
   return (
     <Container>
       <Content>
         <Header>
           <h2>Create a post</h2>
           <button>
-            <img src="/images/close-icon.png"></img>
+            <img src="/images/close-icon.png" />
           </button>
         </Header>
-
         <SharedContent>
           <UserInfo>
-            <img src="/images/user.svg" alt="" />
+            <img src="/images/user.svg" alt="" /> Name
           </UserInfo>
+
+          <textarea
+            value={editorText}
+            onChange={(e) => setEditorText(e.target.value)}
+          ></textarea>
         </SharedContent>
+        <ShareCreation>
+          <AttatchAssets>
+            <AssetButton>
+              <img style={{ color: "gray" }} src="/images/share-img.png"></img>
+            </AssetButton>
+            <AssetButton>
+              <img src="/images/share-vid.png"></img>
+            </AssetButton>
+          </AttatchAssets>
+          <ShareComment>
+            <AssetButton>
+              <img src="/images/share-comment.png" alt="" /> Anyopne
+            </AssetButton>
+          </ShareComment>
+
+          <PostButton>Post</PostButton>
+        </ShareCreation>
       </Content>
     </Container>
   );
@@ -68,7 +92,81 @@ const Header = styled.div`
   }
 `;
 
-const SharedContent = styled.div``;
-const UserInfo = styled.div``;
+const SharedContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+  overflow-y: auto;
+  vertical-align: baseline;
+  background: transparent;
+  padding: 8px 12px;
+`;
+
+const UserInfo = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 12px 14px;
+  svg,
+  img {
+    width: 48px;
+    height: 48px;
+    background-clip: content-box;
+    border: 2px solid transparent;
+    border-radius: 50%;
+  }
+  span {
+    font-weight: 600;
+    font-size: 16px;
+    line-height: 1.5;
+    margin-left: 5px;
+  }
+`;
+
+const ShareCreation = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 12px 24px 12px 16px;
+`;
+
+const AssetButton = styled.button`
+  display: flex;
+  align-items: center;
+  height: 40px;
+  min-width: auto;
+  color: rgba(0, 0, 0, 0.5);
+`;
+
+const AttatchAssets = styled.div`
+  align-items: center;
+  display: flex;
+  padding-right: 8px;
+  ${AssetButton} {
+    width: 40px;
+  }
+`;
+
+const ShareComment = styled.div`
+  padding-left: 8px;
+  margin-right: auto;
+  border-left: 1px solid rgba(0, 0, 0, 0.15);
+  ${AssetButton} {
+    svg,
+    img {
+      margin-right: 5px;
+    }
+  }
+`;
+
+const PostButton = styled.button`
+  min-width: 60px;
+  border-radius: 20px;
+  padding-left: 16px;
+  padding-right: 16px;
+  background: #0a66c2;
+  color: white;
+  &:hover {
+    background: #004182;
+  }
+`;
 
 export default PostModal;
